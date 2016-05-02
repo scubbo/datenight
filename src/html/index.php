72c31cef6bf986480a99ed2573924c12848e9ed0
Jack Jackson <jackjack@amazon.com>
2016-05-02 17:07:04 +0100

    Registration etc. logic in header

--- src/html/index.php
+++ src/html/index.php
@@ -10,48 +10,8 @@
 <?php
   include '../php/header.php';
 ?>
-<p>Login</p>
-<form>
-  <input type='text' id='usernameLoginInput'></input>
-  <input type='password' id='passwordLoginInput'></input>
-  <input type='button' id='loginButton' value='Login'></input>
-</form>
-<input type='button' id='logoutButton' value='Logout'></input>
 </div>
 
-<script type='text/javascript'>
-  $(document).ready(function() {
-  
-    $('#loginButton').click(function() {
-      $.post(
-        '/cgi-bin/login.py',
-        {
-          'username':$('#usernameLoginInput').val(),
-          'password':$('#passwordLoginInput').val()
-        },
-        function(data) {
-          if (data['status'] == 'SUCCESS') {
-            window.location.pathname = '';
-            window.location.reload();
-          } else {
-            window.alert('Your username or password was wrong');
-          }
-        }
-      );
-    });
-  
-    $('#logoutButton').click(function() {
-      $.post(
-        '/cgi-bin/logout.py',
-        {},
-        function(data) {
-          window.location.pathname = '';
-          window.location.reload();
-        }
-      );
-    });
-  });
-</script>
 
 </body>
 </html>
--- src/php/header.php
+++ src/php/header.php
@@ -3,9 +3,54 @@
 <div id="rightOfHeader">
 <?php
 if(!isset($_COOKIE['sessionId'])) {
-  echo "<a href='/register'>Sign up</a>";
-  echo "Sign In";
+  echo <<< EOT
+<a href='/register'>Sign up</a>
+<form>
+  <input type='text' id='usernameLoginInput'></input>
+  <input type='password' id='passwordLoginInput'></input>
+  <input type='button' id='loginButton' value='Login'></input>
+</form>
+<script type='text/javascript'>
+  $(document).ready(function() {
+  
+    $('#loginButton').click(function() {
+      $.post(
+        '/cgi-bin/login.py',
+        {
+          'username':$('#usernameLoginInput').val(),
+          'password':$('#passwordLoginInput').val()
+        },
+        function(data) {
+          if (data['status'] == 'SUCCESS') {
+            window.location.pathname = '';
+            window.location.reload();
+          } else {
+            window.alert('Your username or password was wrong');
+          }
+        }
+      );
+    })
+  });
+</script>
+EOT;
 } else {
+  echo <<< EOT
+<input type='button' id='logoutButton' value='Logout'></input>
+<script type='text/javascript'>
+  $(document).ready(function() {
+    $('#logoutButton').click(function() {
+      $.post(
+        '/cgi-bin/logout.py',
+        {},
+        function(data) {
+          window.location.pathname = '';
+          window.location.reload();
+        }
+      );
+    });
+  });
+</script>
+EOT;
   echo "sessionId is " . $_COOKIE['sessionId'];
 }
 ?>
