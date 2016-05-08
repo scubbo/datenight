4238d3e4e010a9791209595a5ca99fb3cf8a7d72
Jack Jackson <jackjack@amazon.com>
2016-05-08 20:48:38 +0100

    First steps of redesign
    Still need to implement register page

--- src/html/index.php
+++ src/html/index.php
@@ -3,41 +3,14 @@
 <?php
   include '../php/headImport.php';
 ?>
+<link rel='stylesheet' type='text/css' href='/assets/landingPage.css' />
 </head>
 
 <body>
 <div id="main">
-<?php
-  include '../php/header.php';
-?>
-<div id='body'>
-<?php
-if(isset($_COOKIE['sessionId'])) {
-echo <<< EOT
-<script type='text/javascript'>
-$.post(
-  '/cgi-bin/getUserData.py',
-  {
-    'sessionId': '{$_COOKIE['sessionId']}'
-  },
-  function(data) {
-    $('#firstNameSpan').text(data.firstName);
-  }
-);
-</script>
-EOT;
-echo <<< EOT
-<p>Hello <span id='firstNameSpan'></span>, you are logged in!</p>
-<img src='http://tgj.roccamedia.netdna-cdn.com/wp-content/uploads/2015/12/london1-1200x510.jpg'>
-EOT;
-} else {
-echo <<< EOT
-<p>This is the front page of Datenight!</p>
-<q>Wow, Datenight is, like, totally the best!</q> - A satisfied customer
-<q>Datenight rocks my socks</q> - Another satisfied customer
-EOT;
-}
-?>
+<div id='centred'>
+<a href='register'><span id='mainDatenightText' class='neonGlow'>Datenight</span></a>
+<a href='register'><span id='subheadingText' class='neonGlow'>Plan for the Unknown...</span></a>
 </div>
 </div>
 
--- src/html/register/index.php
+++ src/html/register/index.php
@@ -7,58 +7,10 @@
 
 <body>
 <div id="main">
-<?php
-  if(isset($_COOKIE['sessionId'])) {
-    echo "<script type='text/javascript'>$(document).ready(function(){window.location='/';});</script>"; // i.e. - redirect back to main page!
-  } else {
-    echo <<< EOT
-<form>
-  <h3>Login details</h3>
-  <label for='usernameInput'>Username:</label><input type='text' id='usernameInput'></input>
-  <label for='passwordInput'>Password:</label><input type='password' id='passwordInput'></input>
-  <h3>Person details</h3>
-  <label for='firstNameInput'>First Name:</label><input type='text' id='firstNameInput'></input>
-  <label for='lastNameInput'>Last Name:</label><input type='text' id='lastNameInput'></input>
-  <h3>Interests</h3>
-  <p>Check all the below that you are interested in:</p>
-  <input type='checkbox' name='likesDancing' id='likesDancingCheckBox'>Dancing</input>
-  <input type='checkbox' name='likesDrinking' id='likesDrinkingCheckBox'>Drinking</input>
-  <input type='checkbox' name='likesMusicals' id='likesMusicalsCheckBox'>Musicals</input>
-  
-  <input type='button' id='submitButton' value='Register'></input>
-</form>
-
-<script type='text/javascript'>
-  $(document).ready(function() {
-    $('#submitButton').click(function() {
-      $.post(
-        '/cgi-bin/register.py',
-        {
-          'username':$('#usernameInput').val(),
-          'password':$('#passwordInput').val(),
-          'firstName':$('#firstNameInput').val(),
-          'lastName':$('#lastNameInput').val(),
-          'likesDancing':$('#likesDancingCheckBox').is(':checked'),
-          'likesDrinking':$('#likesDrinkingCheckBox').is(':checked'),
-          'likesMusicals':$('#likesMusicalsCheckBox').is(':checked')
-        },
-        function(data) {
-          if (data.status == 'SUCCESS') {
-            window.location='/';
-          } else {
-            if (data.failure_code == 'DUPLICATE_USERNAME') {
-              alert('That username has already been taken. Try another one');
-            } else {
-              alert('An unknown error occurred');
-            }
-          }
-        }
-      )
-    });
-  });
-</script>
-EOT;
-  }
-?>
+<div id="body">
+<h1>OK, let's get to know a bit about you...</h1>
+<p>This page will have multiple questions to find out more about the customer</p>
 </div>
+</div>
+</body>
 </html>
