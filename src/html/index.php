38975b0983cc50f6244c5add3d77f6592e581cc4
Jack Jackson <jackjack@amazon.com>
2016-05-11 21:51:33 +0100

    Stylistic tweaks

--- src/html/index.php
+++ src/html/index.php
@@ -1,19 +1,11 @@
 <html>
 <head>
-<?php
-  include '../php/headImport.php';
-?>
 <link rel='stylesheet' type='text/css' href='/assets/landingPage.css' />
 </head>
 
 <body>
-<div id="main">
-<div id='centred'>
 <a href='register'><span id='mainDatenightText' class='neonGlow'>Datenight</span></a>
 <a href='register'><span id='subheadingText' class='neonGlow'>Plan for the Unknown...</span></a>
-</div>
-</div>
-
 
 </body>
 </html>
