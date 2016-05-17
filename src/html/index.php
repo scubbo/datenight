b32d316fedbd401917a77538bc325eac3c758dae
Jack Jackson <jackjack@amazon.com>
2016-05-17 19:02:05 +0100

    Fade-in on landing page

--- src/html/index.php
+++ src/html/index.php
@@ -1,11 +1,21 @@
 <html>
 <head>
+<?php
+  include '../php/headImport.php';
+?>
 <link rel='stylesheet' type='text/css' href='/assets/landingPage.css' />
 </head>
 
 <body>
 <a href='register'><span id='mainDatenightText' class='neonGlow'>DateNight</span></a>
 <a href='register'><span id='subheadingText' class='neonGlow'>Plan for the Unknown...</span></a>
+
+<script type='text/javascript'>
+  $(document).ready(function() {
+    setTimeout(function() {$('#mainDatenightText').fadeIn(1000);}, 100);
+    setTimeout(function() {$('#subheadingText').fadeIn(1000);}, 1500);
+  });
+</script>
 
 </body>
 </html>
