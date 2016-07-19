8c11bc232e5cf505e6f1155c74186707257a8b39
Jack Jackson <jackjack@amazon.com>
2016-07-19 05:54:52 +0100

    Add common styling on final page

--- src/html/alldone/index.php
+++ src/html/alldone/index.php
@@ -7,8 +7,12 @@
 </head>
 
 <body>
+<div id='main'>
+<div id='body'>
 <p id='text1'>OK, thanks - you're all done!</p>
 <p id='text2'>We'll be in touch shortly to sort payment and other details.</p>
 <p id='text3'>We look forward to speaking with you!</p>
+</div>
+</div>
 </body>
 </html>
